82f1b7f34679de5d80b47074a74b8300bf8e324e
Axel Haustant <noirbizarre@gmail.com>
2012-11-21 21:19:24 +0100

    Added warnings about reload/force-reload support

--- fabtools/service.py
+++ fabtools/service.py
@@ -79,7 +79,6 @@ def restart(service):
     run_as_root('service %(service)s restart' % locals())
 
 
-
 def reload(service):
     """
     Reload a service.
@@ -111,6 +110,6 @@ def force_reload(service):
 
     .. warning::
 
-        The service needs to support the ``force-reload`` operation.
+        The service need to support the ``force-reload`` operation.
     """
     run_as_root('service %(service)s force-reload' % locals())
